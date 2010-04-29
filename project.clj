193011ae2717b692cb1767adccce6e75cc72f854
zkim <zachary.kim@gmail.com>
2010-04-28 23:58:55 -0600

    Switched verson to 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject init-script-test "1.0.0-SNAPSHOT"
+(defproject init-script-test "0.1.0"
   :description "Test project for leiningen-init-script"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]

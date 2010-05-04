eae0592c79b6189afc0a5490515556ee35b09ae1
zkim <zachary.kim@gmail.com>
2010-05-04 01:10:14 -0600

    Added example of java_opts, properties to project lis-opts

--- project.clj
+++ project.clj
@@ -3,5 +3,12 @@
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]
   :dev-dependencies [[leiningen-init-script "0.1.0"]]
+  :lis-opts {:properties {:clj-config.env "dev"
+			  :java.library.path "/some/dir"
+			  :init.script.test.prop "test with spaces"}
+	     :java-opts ["-server"
+			 "-Xms256M"
+			 "-Xmx512M"
+			 "-XX:MaxPermSize=128M"]}
   :main main)
 
--- src/main.clj
+++ src/main.clj
@@ -4,5 +4,6 @@
 (defn -main [& args]
   (doseq [i (range 100000000)]
     (println (str "Hello from test-init-script " (System/currentTimeMillis)))
+    (println (str "System property init.script.test.prop = " (System/getProperty "init.script.test.prop")))
     (Thread/sleep 1000)))
 
